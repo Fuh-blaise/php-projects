d764068b2fb3de7e8713cd471d38981d46fcc763
Fuh-blaise <88884384+Fuh-blaise@users.noreply.github.com>
2023-06-16 02:58:12 +0100

    Delete fo_rm directory

--- fo_rm/partials/footer.php
+++
@@ -1,9 +0,0 @@
-<div class="footer">
-        <div class="wrapper">
-            <p class="text-center">2021 All Rights Reserve,Basic login system
-                <a href="">by Fuh blaise ambe </a></p>
-        </div>
-        
-    </div>
-</body>
-</html>  
